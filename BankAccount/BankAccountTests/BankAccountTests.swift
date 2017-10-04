1471c386a80022ba5e7d186c89677ecef50bd4d8
mike011 <mike_ch_1@hotmail.com>
2017-10-04 10:20:54 -0400

    invalid first name test failing

--- BankAccount/BankAccountTests/BankAccountTests.swift
+++ BankAccount/BankAccountTests/BankAccountTests.swift
@@ -6,8 +6,15 @@ class BankAccountTests: XCTestCase {
     private let validLastName = "Hill"
     private let validEmail = "a.hill@abcfinancial.com"
 
+    private let invalidFirstName = "A"
+
     func testAccountOwner_ValidFirstName_ValidLastName_ValidEmail_CanBeInstantiated() {
         let accountOwner = AccountOwner(firstName: validFirstName, lastName: validLastName, email: validEmail)
         XCTAssertNotNil(accountOwner)
+    }
+
+    func testAccountOwner_InvalidFirstName_ValidFirstName_ValidEmail_CanNotBeInstantiated() {
+        let accountOwner = AccountOwner(firstName: invalidFirstName, lastName: validLastName, email: validEmail)
+        XCTAssertNil(accountOwner)
     }
 }
