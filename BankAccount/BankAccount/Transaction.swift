7bac87f9aba50269700e316869f258b32eda6f6b
mike011 <mike_ch_1@hotmail.com>
2017-10-11 21:30:27 -0400

    date tests

--- BankAccount/BankAccount/Transaction.swift
+++ BankAccount/BankAccount/Transaction.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 class DescriptionValidator {
     func validate(_ description: String) -> Bool {
         if(description.isEmpty || description.count > 20) {
@@ -9,15 +11,17 @@ class DescriptionValidator {
 class Transaction {
 
     let description:String
+    let date:Date
 
-    convenience init?(description: String) {
-        self.init(description: description, descriptionValidator: DescriptionValidator())
+    convenience init?(description: String, date: Date) {
+        self.init(description: description, descriptionValidator: DescriptionValidator(), date: date)
     }
 
-    init?(description: String, descriptionValidator: DescriptionValidator) {
+    init?(description: String, descriptionValidator: DescriptionValidator, date: Date) {
         if !descriptionValidator.validate(description) {
             return nil
         }
         self.description = description
+        self.date = Date()
     }
 }
--- BankAccount/BankAccountTests/TransactionTests.swift
+++ BankAccount/BankAccountTests/TransactionTests.swift
@@ -7,27 +7,35 @@ class CustomDescriptionValidator: DescriptionValidator {
 }
 class TransactionTests: XCTestCase {
 
+    let validDescription = "valid"
+    let validDate = Date()
+
     func testDescription_Empty_CanNotBeInstantiated() {
         let description = ""
-        let transaction = Transaction(description: description)
+        let transaction = Transaction(description: description, date: validDate)
         XCTAssertNil(transaction)
     }
 
     func testDescription_GreaterThen20Chars_CanNotBeInstantiated() {
         let description = "JscxTD4oBZZzSXRc4OhXH"
-        let transaction = Transaction(description: description)
+        let transaction = Transaction(description: description, date: validDate)
         XCTAssertNil(transaction)
     }
 
     func testDescription_CustomValidator_CanBeInstantiated() {
         let description = ""
-        let transaction = Transaction(description: description, descriptionValidator: CustomDescriptionValidator())
+        let transaction = Transaction(description: description,  descriptionValidator: CustomDescriptionValidator(), date: validDate)
         XCTAssertNotNil(transaction)
     }
 
     func testDescription_Valid_IsSet() {
-        let description = "valid"
-        let transaction = Transaction(description: description)
+        let transaction = Transaction(description: validDescription, date: validDate)
         XCTAssertEqual("valid", transaction?.description)
+    }
+
+    func testDate_Valid_IsSet() {
+        let date = Date()
+        let transaction = Transaction(description: validDescription, date: date)
+        XCTAssertNotNil(transaction?.date)
     }
 }
